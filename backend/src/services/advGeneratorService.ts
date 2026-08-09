7cf403b5a9eeeea48795a879f0103f9d93a0f158
Robert Voong <rvoong1408@gmail.com>
2026-08-09 20:37:10 +0200

    generate adventure service with Qwen

---
+++ backend/src/services/advGeneratorService.ts
@@ -0,0 +1,28 @@
+import { InferenceClient } from "@huggingface/inference";
+
+export class advGeneration {
+    
+    async generate(caracteristics: string[]) {
+        const SYSTEM_PROMPT: string = `Eres un creador de viajes y el usuario te da una lista de caracteristicas con las que tienes que crear 
+                                        tu viaje. No hace falta que incluyas todas las caracteristicas y tampoco es necesario que solo tengo las 
+                                        caracteristicas que te proporcionen. Dentro del viaje puedes recomendar paises y actividades. Formatea tu
+                                        respuesta a markdown para que sea mas facil el renderizado en la web ` 
+        const message = caracteristics.join(", ");
+        const hf = new InferenceClient(process.env.HUGGING_FACE_API_KEY);
+            const response = await hf.chatCompletion({
+                model: "Qwen/Qwen2.5-3B-Instruct",
+                messages:[
+                    { role: "system", content: SYSTEM_PROMPT},
+                    { role: "user", content: `I want you to make me a trip that takes into account: ${message}`}
+                ],
+                 max_tokens: 1500
+            });
+            const choice = response?.choices?.[0];
+            return choice?.message?.content ?? "";
+        
+
+    }
+
+
+    
+}
