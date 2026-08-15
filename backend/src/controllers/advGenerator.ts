3f3233056840234967e5a5e8889893798a2e6362
Robert Voong <rvoong1408@gmail.com>
2026-08-15 23:31:41 +0200

    MySQL connection

--- backend/src/controllers/advGenerator.ts
+++ backend/src/controllers/advGenerator.ts
@@ -1,9 +1,8 @@
 import { Request, Response, NextFunction} from "express"
 import { advGeneratorService } from "../services/index.js";
 import { GenerationError } from "../types/errors.types.js";
-import { Adventure } from "../types/object.types.js";
+import { Adventure } from "../types/object.types.js"; 
 
-//La idea es que la request tenga la informacion del viaje y que esta informacion se pase al servicio para ejecutar el prompt
 const advGenerator = async (req: Request, res:Response, next: NextFunction) => {
     //antes de nada extraeremos la informacion del body que vendra como un array y lo uniremos en un string
     try {
