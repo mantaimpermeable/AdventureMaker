2d9e484fda9045407419e4f63b25cf76bdca2143
Robert Voong <rvoong1408@gmail.com>
2026-08-17 12:16:25 +0200

    script init:db functional

--- backend/src/scripts/init_db.ts
+++ backend/src/scripts/init_db.ts
@@ -1,4 +1,4 @@
-import { pool } from "../database/Database.js";
+import mysql from 'mysql2/promise'
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -7,15 +7,21 @@ dotenv.config();
 const initiateDB = async () => {
     try {
         const dbName = process.env.DB_NAME;
+        //create a connection instead of accesing database
+        const connection = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD
+        });
         //Caracter set utf8mb4 == caracters of 1-4 bytes (emojis)
         //Collate--rules of text compariSON, utf8mb4 (1-4 bytes caracters); unicode--Unicode standar; ci--Case insensitive
-        await pool.query(`CREATE DATABASE IF NOT EXISTS \`${dbName}\` 
+        await connection.query(`CREATE DATABASE IF NOT EXISTS ??
                          CHARACTER SET utf8mb4 
-                         COLLATE utf8mb4_unicode_ci`);
+                         COLLATE utf8mb4_unicode_ci`, [dbName]);
         
         console.log(`Base de datos ${dbName} lista`);
         //use the database
-        await pool.query(`Use \'${dbName}\'`);
+        await connection.query(`USE ??`, [dbName]);
         console.log(`Using ${dbName} schema`);
 
         //process exit to kill the process, return would only exit the function but let the process
--- notas.md
+++ notas.md
@@ -1,7 +1,7 @@
 <ul></ul>
     <li>ts-mysql-migrate necesita la conexion a base de datos sin promise</li>
     <li>para las operaciones tipo query que estan dentro de async/await si necesitamos promise</li>
-    <li></li>
+    <li>Para operaciones sencillas en una base de datos podemos crear una connection en vez de una pool</li>
     <li></li>
     <li></li>
 <ul></ul>
